79c1507d578557c4d5e06a82c8ac41b56da8ec1c
Max Sokolov <i@maxsokolov.net>
2016-07-04 20:34:05 +0300

    add xcode project

---
+++ Cribble/Cribble.h
@@ -0,0 +1,19 @@
+//
+//  Cribble.h
+//  Cribble
+//
+//  Created by Max Sokolov on 04/07/16.
+//  Copyright © 2016 Cribble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cribble.
+FOUNDATION_EXPORT double CribbleVersionNumber;
+
+//! Project version string for Cribble.
+FOUNDATION_EXPORT const unsigned char CribbleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cribble/PublicHeader.h>
+
+
