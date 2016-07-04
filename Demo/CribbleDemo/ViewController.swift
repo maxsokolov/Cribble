21e1821ed4f06493478a8cbd1a074814e07f497e
Max Sokolov <i@maxsokolov.net>
2016-07-04 20:38:51 +0300

    add demo project

---
+++ Demo/CribbleDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CribbleDemo
+//
+//  Created by Max Sokolov on 04/07/16.
+//  Copyright © 2016 Cribble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
